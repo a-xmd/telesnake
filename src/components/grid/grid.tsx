d53c6e22a36ba97e7a2144bc64375acc2ee61717
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-09-30 23:08:53 +0200

    feat: add to base page

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -5,6 +5,7 @@ import { Block } from '../block/block.tsx'
 interface TeletekstChar {
   char: string
   color?: 'red' | 'blue'
+  background?: 'green'
 }
 
 export const Grid = () => {
@@ -157,99 +158,31 @@ export const Grid = () => {
 
       ...Array(26).fill({ char: '', color: 'green' }),
     ],
-    ...[
-      ...Array(14).fill({ char: '', color: 'green' }),
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-      {
-        char: '',
-      },
-    ],
-    ...Array(800).fill(null),
+    ...[...Array(14).fill({ char: '', color: 'green' })],
   ])
 
   return (
     <div className={styles.grid}>
       {values.map((value, index) => {
         return (
-          <Block key={`cell-${index}`} color={value?.color}>
+          <Block
+            key={`cell-${index}`}
+            color={value?.color}
+            background={value?.background}
+          >
             {value?.char ?? '·'}
           </Block>
         )
       })}
+      <div className={styles.bottomDivider}>
+        <a href="#">© AXMD - P.T. SEPT. 2024</a>
+      </div>
+      <div className={styles.topscores}>topscores</div>
+      <div className={styles.over}>over</div>
+      <div className={styles.instellingen}>instellingen</div>
+      <a className={styles.bron} href="https://github.com/a-xmd/telesnake">
+        bron
+      </a>
     </div>
   )
 }
