a0fdbb11d4853386ee510c93205fbd10282a92c2
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-09-30 21:05:32 +0200

    wip: base layout

--- src/components/block/block.tsx
+++ src/components/block/block.tsx
@@ -1,5 +1,27 @@
 import { type FC, PropsWithChildren } from 'react'
+import clsx from 'clsx'
+import styles from './block.module.css'
 
-export const Block: FC<PropsWithChildren> = ({ children }) => {
-  return <span>{children}</span>
+interface BlockProps {
+  color?: 'red' | 'blue' | 'green'
+  background?: 'green'
+}
+
+export const Block: FC<PropsWithChildren<BlockProps>> = ({
+  children,
+  color,
+  background,
+}) => {
+  return (
+    <span
+      className={clsx(styles.block, {
+        [styles.red]: color === 'red',
+        [styles.blue]: color === 'blue',
+        [styles.green]: color === 'green',
+        [styles.bgGreen]: background === 'green',
+      })}
+    >
+      {children}
+    </span>
+  )
 }
--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -1,175 +1,242 @@
 import styles from './grid.module.css'
 import { useState } from 'react'
+import { Block } from '../block/block.tsx'
+
+interface TeletekstChar {
+  char: string
+  color?: 'red' | 'blue'
+}
 
 export const Grid = () => {
-  const [values] = useState(() => [
+  const [values] = useState<(TeletekstChar | null)[]>(() => [
     ...[
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      ...Array(27).fill({ char: '', color: 'green' }),
     ],
     ...[
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      ' ',
-      ' ',
-      'T',
-      ' ',
-      ' ',
-      'E',
-      ' ',
-      ' ',
-      'L',
-      ' ',
-      ' ',
-      'E',
-      ' ',
-      ' ',
-      'S',
-      ' ',
-      ' ',
-      'N',
-      ' ',
-      ' ',
-      'A',
-      ' ',
-      ' ',
-      'K',
-      ' ',
-      ' ',
-      'E',
-      ' ',
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: ' ',
+      },
+      {
+        char: ' ',
+      },
+      ...Array.from(Array.from('TELESNAKE').join('  ')).map((item) => ({
+        char: item,
+        color: 'green',
+      })),
+      {
+        char: ' ',
+      },
     ],
     ...[
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+        color: 'red',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+        color: 'green',
+      },
+
+      ...Array(26).fill({ char: '', color: 'green' }),
     ],
     ...[
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
+      ...Array(14).fill({ char: '', color: 'green' }),
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
+      {
+        char: '',
+      },
     ],
     ...Array(800).fill(null),
   ])
@@ -177,7 +244,11 @@ export const Grid = () => {
   return (
     <div className={styles.grid}>
       {values.map((value, index) => {
-        return <span key={`cell-${index}`}>{value ?? '·'}</span>
+        return (
+          <Block key={`cell-${index}`} color={value?.color}>
+            {value?.char ?? '·'}
+          </Block>
+        )
       })}
     </div>
   )
