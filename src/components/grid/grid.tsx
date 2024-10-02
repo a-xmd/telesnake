ed16322c3d5cce7eada67fc3791b57e65dfba7a0
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-10-02 15:57:15 +0200

    feat: add base logic

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -2,17 +2,7 @@ import styles from './grid.module.css'
 import { Header } from '../header/header.tsx'
 import { Footer } from '../footer/footer.tsx'
 import { GameGrid } from '../game/game-grid/game-grid.tsx'
-import { Block } from '../block/block.tsx'
-import { type Position } from '../../types.ts'
-
-const Snake = () => {
-  const positions: Position[] = [
-    { x: 3, y: 5 },
-    { x: 4, y: 5 },
-    { x: 5, y: 5 },
-  ]
-  return positions.map((position) => <Block position={position}> </Block>)
-}
+import { Snake } from '../snake/snake.tsx'
 
 export const Grid = () => {
   return (
