c713c66e6eddf440233ab318ed7bb0013ea92fff
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-10-02 16:34:09 +0200

    fix: god save jetbrains

---
+++ src/hooks/use-interval.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react'
+
+export const useInterval = (callback: () => void, delay: number | null) => {
+  const intervalRef = useRef<number | null>(null)
+  const savedCallback = useRef(callback)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    const tick = () => savedCallback.current()
+    if (typeof delay === 'number') {
+      intervalRef.current = window.setInterval(tick, delay)
+
+      return () => {
+        if (intervalRef.current) {
+          window.clearInterval(intervalRef.current)
+        }
+      }
+    }
+  }, [delay])
+  return intervalRef
+}
