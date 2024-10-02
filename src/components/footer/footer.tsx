c102aaea2d3b78c66d087532f3f8f948f83fb9d5
a-xmd <62752968+a-xmd@users.noreply.github.com>
2024-10-02 11:30:36 +0200

    chore: clean up (#1)

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,17 @@
+import styles from './footer.module.css'
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.bottomDivider}>
+        <a href="#">© AXMD - P.T. SEPT. 2024</a>
+      </div>
+      <div className={styles.topscores}>topscores</div>
+      <div className={styles.debug}>debug</div>
+      <div className={styles.instellingen}>instellingen</div>
+      <a className={styles.bron} href="https://github.com/a-xmd/telesnake">
+        bron
+      </a>
+    </div>
+  )
+}
